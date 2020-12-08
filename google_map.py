3b58ca23cb8fdabea699ebf0365bf7df7377c2bb
Yangyoonjoon <64205127+Yangyoonjoon@users.noreply.github.com>
2020-12-08 22:35:02 +0900

    It works on my machine

--- google_map.py
+++ google_map.py
@@ -2,14 +2,17 @@ import requests
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 
-def googleMap(place, w, h):
+def googleMap(place, w, h, z, t):
     URL = f'https://maps.googleapis.com/maps/api/staticmap?'
     PLACE = f'center={place}'
     SIZE = '&size=640x640&scale=2'
     # 키 필요
     KEY = '&key='
+    ZOOM = f'&zoom={z}'
+    TYPE = f'&maptype={t}'
+    MARKER = f'&markers=color:0xFF0000|label:A|{place}'
 
-    MSG = URL + PLACE + SIZE + KEY
+    MSG = URL + PLACE + SIZE + ZOOM + TYPE + MARKER + KEY
 
     # 구글로 요청
     resp = requests.get(MSG)
--- main.py
+++ main.py
@@ -8,6 +8,16 @@ class Form(QWidget):
         super().__init__()
         loadUi('form.ui', self)
 
+        # 콤보박스 초기화
+        #for i in range(1, 21):
+        #    self.cmb.addItem(str(i))
+
+        n = [str(i+1) for i in range(20)]
+        self.cmb.addItems(n)
+
+        types = ('roadmap', 'satellite', 'terrain', 'hybrid')
+        self.map.addItems(types)
+
         # 시그널
         self.btn.clicked.connect(self.clickBtn)
 
@@ -15,7 +25,9 @@ class Form(QWidget):
         place = self.le.text()
         w = self.pic.width()
         h = self.pic.height()
-        img = googleMap(place, w, h)
+        z = self.cmb.currentText()
+        t = self.map.currentText()
+        img = googleMap(place, w, h, z, t)
         self.pic.setPixmap(img)
 
 if __name__ == '__main__':
